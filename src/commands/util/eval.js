e91bd9e50379ed84c1522796b2f3edfb43931be1
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2016-10-22 13:49:16 -0400

    Fixed sensitivePattern visibility in eval

--- src/commands/util/eval.js
+++ src/commands/util/eval.js
@@ -126,7 +126,7 @@ module.exports = class EvalCommand extends Command {
 			if(client.token) pattern += escapeRegex(client.token);
 			if(client.email) pattern += (pattern.length > 0 ? '|' : '') + escapeRegex(client.email);
 			if(client.password) pattern += (pattern.length > 0 ? '|' : '') + escapeRegex(client.password);
-			this._sensitivePattern = new RegExp(pattern, 'gi');
+			Object.defineProperty(this, '_sensitivePattern', { value: new RegExp(pattern, 'gi') });
 		}
 		return this._sensitivePattern;
 	}
