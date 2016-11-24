5408d9574c0b3d208d481e39e9ac376ca48ad497
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2016-11-24 02:20:27 -0500

    Tiny cleanup

--- src/errors/command-format.js
+++ src/errors/command-format.js
@@ -9,11 +9,13 @@ class CommandFormatError extends FriendlyError {
 	 * @param {CommandMessage} msg - The command message the error is for
 	 */
 	constructor(msg) {
-		super(`Invalid command format. Use ${msg.anyUsage(
-			`help ${msg.command.name}`,
-			msg.guild ? undefined : null,
-			msg.guild ? undefined : null
-		)} for information.`);
+		super(
+			`Invalid command format. Use ${msg.anyUsage(
+				`help ${msg.command.name}`,
+				msg.guild ? undefined : null,
+				msg.guild ? undefined : null
+			)} for information.`
+		);
 		this.name = 'CommandFormatError';
 	}
 }
