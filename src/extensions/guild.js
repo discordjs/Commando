8701d0fa686ef73c4ff6abcff2af3f7e62f6657c
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2016-11-26 00:58:30 -0500

    Mark GuildExtension.applyToClass as private

--- src/extensions/guild.js
+++ src/extensions/guild.js
@@ -104,6 +104,7 @@ class GuildExtension {
 	/**
 	 * Applies the interface to a class prototype
 	 * @param {function} target - The constructor function to apply to the prototype of
+	 * @private
 	 */
 	static applyToClass(target) {
 		for(const prop of [
