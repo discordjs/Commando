e4520907f98e03cd4311097525dee1aed9eaefd4
Adam Gaskins <adam@theadamgaskins.com>
2016-12-13 18:51:24 -0500

    Added plugin to main export

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const discord = require('discord.js');
 module.exports = {
 	Client: require('./client'),
 	CommandoClient: require('./client'),
+	CommandPlugin: require('./commands/plugin'),
 	Command: require('./commands/base'),
 	CommandArgument: require('./commands/argument'),
 	CommandGroup: require('./commands/group'),
