f62469550c7c0f8c8d9f35e83840c3c58e7fe328
Crawl <icrawltogo@gmail.com>
2017-05-16 04:42:54 +0200

    maxMatche is now max

--- src/commands/argument.js
+++ src/commands/argument.js
@@ -156,7 +156,7 @@ class Argument {
 
 			// Get the user's response
 			const responses = await msg.channel.awaitMessages(msg2 => msg2.author.id === msg.author.id, {
-				maxMatches: 1,
+				max: 1,
 				time: wait
 			});
 
@@ -253,7 +253,7 @@ class Argument {
 
 				// Get the user's response
 				const responses = await msg.channel.awaitMessages(msg2 => msg2.author.id === msg.author.id, {
-					maxMatches: 1,
+					max: 1,
 					time: wait
 				});
 
