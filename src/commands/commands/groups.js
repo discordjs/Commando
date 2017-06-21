de004fdb28df440e6841d16a64dcd81d44ca53b3
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2017-06-20 20:10:35 -0400

    Removed redundant permission checks

--- src/commands/commands/groups.js
+++ src/commands/commands/groups.js
@@ -15,7 +15,6 @@ module.exports = class ListGroupsCommand extends Command {
 	}
 
 	hasPermission(msg) {
-		if(this.client.options.selfbot) return true;
 		if(!msg.guild) return this.client.isOwner(msg.author);
 		return msg.member.hasPermission('ADMINISTRATOR') || this.client.isOwner(msg.author);
 	}
