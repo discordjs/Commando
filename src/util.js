2d8af4f27521608199d7bf444e86a354db5ed973
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2017-09-03 20:33:04 +0000

    shouldn't mess with that

--- src/util.js
+++ src/util.js
@@ -1,5 +1,5 @@
 function disambiguation(items, label, property = 'name') {
-	const itemList = items.map(item => `'${(property ? item[property] : item).replace(/ /g, '\xa0')}'`).join(',   ');
+	const itemList = items.map(item => `"${(property ? item[property] : item).replace(/ /g, '\xa0')}"`).join(',   ');
 	return `Multiple ${label} found, please be more specific: ${itemList}`;
 }
 
