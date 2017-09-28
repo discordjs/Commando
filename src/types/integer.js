aa49f15b1e0c39a9ab0d555f2ac4706b5db5e374
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2017-09-28 00:41:25 +0000

    Make min/max more descriptive

--- src/types/integer.js
+++ src/types/integer.js
@@ -7,9 +7,14 @@ class IntegerArgumentType extends ArgumentType {
 
 	validate(value, msg, arg) {
 		const int = Number.parseInt(value);
-		return !Number.isNaN(int) &&
-			(arg.min === null || typeof arg.min === 'undefined' || int >= arg.min) &&
-			(arg.max === null || typeof arg.max === 'undefined' || int <= arg.max);
+		if(!Number.isNaN(int)) return false;
+		if(arg.min !== null && typeof arg.min !== 'undefined' && int <= arg.min) {
+			return `Please enter a number above ${arg.min}.`;
+		}
+		if(arg.max !== null && typeof arg.max !== 'undefined' && int >= arg.max) {
+			return `Please enter a number below ${arg.max}.`;
+		}
+		return true;
 	}
 
 	parse(value) {
