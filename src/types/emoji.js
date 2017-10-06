1a519b8145d19526b4d2d1dadd7d15320bb1b490
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2017-10-06 13:50:27 +0000

    eslint

--- src/types/emoji.js
+++ src/types/emoji.js
@@ -9,29 +9,29 @@ class EmojiArgumentType extends ArgumentType {
 
 	validate(value, msg) {
 		const matches = value.match(/^(?:<:([a-zA-Z0-9_]+):)?([0-9]+)>?$/);
-		if (matches && msg.client.emojis.has(matches[2])) return true;
-		if (!msg.guild) return false;
+		if(matches && msg.client.emojis.has(matches[2])) return true;
+		if(!msg.guild) return false;
 		const search = value.toLowerCase();
 		let emojis = msg.guild.emojis.filterArray(nameFilterInexact(search));
-		if (emojis.length === 0) return false;
-		if (emojis.length === 1) return true;
+		if(emojis.length === 0) return false;
+		if(emojis.length === 1) return true;
 		const exactEmojis = emojis.filter(nameFilterExact(search));
-		if (exactEmojis.length === 1) return true;
-		if (exactEmojis.length > 0) emojis = exactEmojis;
-		return emojis.length <= 15
-			? `${disambiguation(emojis.map(emoji => escapeMarkdown(emoji.name)), 'emojis', null)}\n`
-			: 'Multiple emojis found. Please be more specific.';
+		if(exactEmojis.length === 1) return true;
+		if(exactEmojis.length > 0) emojis = exactEmojis;
+		return emojis.length <= 15 ?
+			`${disambiguation(emojis.map(emoji => escapeMarkdown(emoji.name)), 'emojis', null)}\n` :
+			'Multiple emojis found. Please be more specific.';
 	}
 
 	parse(value, msg) {
 		const matches = value.match(/^(?:<:([a-zA-Z0-9_]+):)?([0-9]+)>?$/);
-		if (matches) return msg.client.emojis.get(matches[2]) || null;
+		if(matches) return msg.client.emojis.get(matches[2]) || null;
 		const search = value.toLowerCase();
 		const emojis = msg.guild.emojis.filterArray(emoji => emoji.name.toLowerCase().includes(search));
-		if (emojis.length === 0) return null;
-		if (emojis.length === 1) return emojis[0];
+		if(emojis.length === 0) return null;
+		if(emojis.length === 1) return emojis[0];
 		const exactEmojis = msg.guild.emojis.filterArray(emoji => emoji.name.toLowerCase() === search);
-		if (exactEmojis.length === 1) return exactEmojis[0];
+		if(exactEmojis.length === 1) return exactEmojis[0];
 		return null;
 	}
 }
