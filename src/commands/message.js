d695f81833d47301d59f49aafb7802211f59ba59
googer@pobox.com <VCevHxzzh3mcbyATu8Qz>
2017-10-23 21:47:22 -0400

    More comment fixes

--- src/commands/message.js
+++ src/commands/message.js
@@ -440,8 +440,8 @@ class CommandMessage {
 		/**
 		 * Emitted when finalizing a command
 		 * @event CommandoClient#commandFinalize
-		 * @param {Command} command - Command that is being run
-		 * @param {CommandMessage} message - Command message that the command is running from (see {@link Command#run})
+		 * @param {Command} command - Command that is being finalized
+		 * @param {CommandMessage} message - Command message that the command is finalized from (see {@link Command#run})
 		 * @param {boolean} fromPattern - Whether the args are pattern matches (see {@link Command#run})
 		 */
 		this.client.emit('commandFinalize', this.command, this, Boolean(this.patternMatches));
