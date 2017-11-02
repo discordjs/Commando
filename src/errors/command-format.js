7669d21d56ee3efd9700c6c8e8025ca5534f5c55
Schuyler Cebulskie <Gawdl3y@gmail.com>
2017-11-02 17:48:37 -0400

    Update command-format.js

--- src/errors/command-format.js
+++ src/errors/command-format.js
@@ -10,11 +10,15 @@ class CommandFormatError extends FriendlyError {
 	 */
 	constructor(msg) {
 		super(
-			`Invalid command format. Please use the proper format, ${msg.usage(
+			`Invalid command usage. The \`${msg.command.name}\` command's accepted format is: ${msg.usage(
 				msg.command.format,
 				msg.guild ? undefined : null,
 				msg.guild ? undefined : null
-			)}.`
+			)}. Use ${msg.usage(
+				`help ${msg.command.name}`,
+				msg.guild ? undefined : null,
+				msg.guild ? undefined : null
+			)} for more information.`
 		);
 		this.name = 'CommandFormatError';
 	}
