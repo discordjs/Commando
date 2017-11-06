7cd316f9d8939d022423b7d6228baf5ce143b0d5
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2017-11-05 21:49:58 -0500

    Fix lint errors

--- src/commands/commands/enable.js
+++ src/commands/commands/enable.js
@@ -1,6 +1,5 @@
-const { oneLine, stripIndents } = require('common-tags');
+const { oneLine } = require('common-tags');
 const Command = require('../base');
-const { disambiguation } = require('../../util');
 
 module.exports = class EnableCommandCommand extends Command {
 	constructor(client) {
