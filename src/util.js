79fb47ee0a63832df90e47cfa7cb0b29a1664ddd
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2017-12-02 13:47:03 -0500

    Fix a massive fatal flaw

--- src/util.js
+++ src/util.js
@@ -7,7 +7,7 @@ function paginate(items, page = 1, pageLength = 10) {
 	const maxPage = Math.ceil(items.length / pageLength);
 	if(page < 1) page = 1;
 	if(page > maxPage) page = maxPage;
-	let startIndex = (page - 1) * pageLength;
+	const startIndex = (page - 1) * pageLength;
 	return {
 		items: items.length > pageLength ? items.slice(startIndex, startIndex + pageLength) : items,
 		page,
