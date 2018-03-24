3d32c816cc48f4be11df1ab65c888356342fd97e
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2018-03-24 17:31:30 -0400

    Add outputting of validate messages to union types

--- src/types/union.js
+++ src/types/union.js
@@ -22,6 +22,8 @@ class ArgumentUnionType extends ArgumentType {
 		let results = this.types.map(type => !type.isEmpty(val, msg, arg) ? type.validate(val, msg, arg) : false);
 		results = await Promise.all(results);
 		if(results.some(valid => valid && typeof valid !== 'string')) return true;
+		const errors = results.filter(valid => typeof valid === 'string');
+		if(errors.length > 0) return errors.join('\n');
 		return false;
 	}
 
