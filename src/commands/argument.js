15d2392fef31465b79cf91b49065c82f0646a744
Jason Khanlar <jason.khanlar@gmail.com>
2018-11-17 13:30:27 -0500

    Adjusted showCancel feature renamed as hidePrompt to prevent prompt from displaying if argument is missing, and to pass an empty string for to command

--- src/commands/argument.js
+++ src/commands/argument.js
@@ -60,7 +60,7 @@ class Argument {
 		 * Whether the cancellation message is displayed for the command
 		 * @type {boolean}
 		 */
-		this.showCancel = 'showCancel' in info ? info.showCancel : true;
+		this.hidePrompt = 'hidePrompt' in info ? info.hidePrompt : false;
 
 		/**
 		 * Error message for when a value is invalid
@@ -174,6 +174,14 @@ class Argument {
 
 		while(!valid || typeof valid === 'string') {
 			/* eslint-disable no-await-in-loop */
+			if(this.hidePrompt) {
+				return {
+					value: '',
+					cancelled: null,
+					prompts,
+					answers
+				};
+			}
 			if(prompts.length >= promptLimit) {
 				return {
 					value: null,
@@ -186,10 +194,10 @@ class Argument {
 			// Prompt the user for a new value
 			prompts.push(await msg.reply(stripIndents`
 				${empty ? this.prompt : valid ? valid : `You provided an invalid ${this.label}. Please try again.`}
-				${this.showCancel ? oneLine`
+				${oneLine`
 					Respond with \`cancel\` to cancel the command.
 					${wait ? `The command will automatically be cancelled in ${this.wait} seconds.` : ''}
-				` : ''}
+				`}
 			`));
 
 			// Get the user's response
@@ -275,18 +283,18 @@ class Argument {
 							"${escaped.length < 1850 ? escaped : '[too long to show]'}".
 							Please try again.
 						`}
-						${this.showCancel ? oneLine`
+						${oneLine`
 							Respond with \`cancel\` to cancel the command, or \`finish\` to finish entry up to this point.
 							${wait ? `The command will automatically be cancelled in ${this.wait} seconds.` : ''}
-						` : ''}
+						`}
 					`));
 				} else if(results.length === 0) {
 					prompts.push(await msg.reply(stripIndents`
 						${this.prompt}
-						${this.showCancel ? oneLine`
+						${oneLine`
 							Respond with \`cancel\` to cancel the command, or \`finish\` to finish entry.
 							${wait ? `The command will automatically be cancelled in ${this.wait} seconds, unless you respond.` : ''}
-						` : ''}
+						`}
 					`));
 				}
 
@@ -396,7 +404,7 @@ class Argument {
 		if(typeof info !== 'object') throw new TypeError('Argument info must be an Object.');
 		if(typeof info.key !== 'string') throw new TypeError('Argument key must be a string.');
 		if(info.label && typeof info.label !== 'string') throw new TypeError('Argument label must be a string.');
-		if(typeof info.prompt !== 'string') throw new TypeError('Argument prompt must be a string.');
+		if(typeof info.prompt !== 'string' && !info.hidePrompt) throw new TypeError('Argument prompt must be a string.');
 		if(info.error && typeof info.error !== 'string') throw new TypeError('Argument error must be a string.');
 		if(info.type && typeof info.type !== 'string') throw new TypeError('Argument type must be a string.');
 		if(info.type && !info.type.includes('|') && !client.registry.types.has(info.type)) {
