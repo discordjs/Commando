6bb98d26787315f33fdc4cb8d5f906588ee6f5ee
Mitchell Dzurisin <madlab96@gmail.com>
2019-03-28 12:55:55 -0500

    finished parse and added base conversion methods

--- src/types/duration.js
+++ src/types/duration.js
@@ -6,6 +6,7 @@ class DurationArgumentType extends ArgumentType {
 	constructor(client) {
 		super(client, 'duration');
 		this.timeIds = new Set(['mo', 'w', 'd', 'h', 'm', 's', 'ms']);
+		this.duration = 0;
 	}
 
 	validate(value) {
@@ -26,9 +27,66 @@ class DurationArgumentType extends ArgumentType {
 	}
 
 	parse(value) {
-		const matches = value.match(/(\d+)\s*([A-Za-z]+)/g);
-		console.log("matches2 " + matches);
+		const MATCHES_ALL = value.match(/(\d+)\s*([A-Za-z]+)/g);
+		var totalTime = 0;
+		// Separate each time group (Xmo, Yw, Zd, ext.)
+		MATCHES_ALL.forEach(dur => {
+			var tempNum = parseInt(dur.match(/(\d+)/g));
+			var tempStr = dur.match(/([A-Za-z]+)/g);
+			// Combine to a single time value (in milliseconds)
+			if(tempNum === 'NaN') totalTime = null; //or mb ignore?
+			else totalTime += (tempNum * determineTimeType(tempStr));
+		});
+		// Return time value unless null
+		if(totalTime != null) {
+			this.duration = totalTime;
+			return this.duration;
+		} else {
+			return null;
+		}
+	}
+	
+	// conversion methods
+	toMonths() {
+		
+	}
+	toWeeks() {
+		
+	}
+	toDays() {
+		
+	}
+	toMinutes() {
+		
+	}
+	toSeconds() {
+		
+	}
+	toMilliseconds() {
+		
+	}
 
+	//method aliases
+	toMo() { return this.toMonths(); }
+	toW() { return this.toMonths(); }
+	toD() { return this.toMonths(); }
+	toD() { return this.toMonths(); }
+	toMo() { return this.toMonths(); }
+	toMo() { return this.toMonths(); }
+
+}
+
+function determineTimeType(str) {
+	// match multipier
+	switch(str) {
+		case 'mo': return (30 * 24 * 60 * 60 * 1000)
+		case 'w': return (7  * 24 * 60 * 60 * 1000)
+		case 'd': return (24 * 60 * 60 * 1000)	
+		case 'h': return (60 * 60 * 1000)
+		case 'm': return (60 * 1000)
+		case 's': return (1000)
+		case 'ms': return (1)
+		default: return null;
 	}
 }
 
