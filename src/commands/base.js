016931393db65fb73b1a8f39204d717e04243f35
mikerophone <47287285+mah51@users.noreply.github.com>
2020-06-06 12:40:11 +0100

    Update src/commands/base.js
    Co-authored-by: Sugden <28943913+NotSugden@users.noreply.github.com>

--- src/commands/base.js
+++ src/commands/base.js
@@ -428,8 +428,8 @@ class Command {
 	 */
 	isUsable(message = null) {
 		if(!message) return this._globalEnabled;
-		if(this.guildOnly && message && !message.guild) return false;
-		if(this.dmOnly && message && message.channel.type !== 'dm') return false;
+		if(this.guildOnly && !message.guild) return false;
+		if(this.dmOnly && message.channel.type !== 'dm') return false;
 		const hasPermission = this.hasPermission(message);
 		return this.isEnabledIn(message.guild) && hasPermission && typeof hasPermission !== 'string';
 	}
