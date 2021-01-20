5f951a52a161df9c23bb2e2a4c827b4cd7294bb6
Carter <45381083+Fyko@users.noreply.github.com>
2021-01-20 16:48:05 -0700

    refert: typings/fix: improve typings (#361)

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -160,6 +160,7 @@ declare module 'discord.js-commando' {
 		public patternMatches: string[] | null;
 		public responsePositions: { [key: string]: number } | null;
 		public responses: { [key: string]: CommandoMessage[] } | null;
+		public readonly guild: CommandoGuild;
 
 		private deleteRemainingResponses(): void;
 		private editCurrentResponse(id: string, options: MessageEditOptions | Exclude<MessageAdditions, MessageAttachment>): Promise<CommandoMessage | CommandoMessage[]>;
