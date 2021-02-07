d2b0b39cc0d6446dcf91c3abc3a7e25a2799d022
Maxim Shamber <36640388+nowifi4u@users.noreply.github.com>
2021-02-07 21:19:14 +0300

    Add emoji format :name: and enforce formed emojis

--- src/types/custom-emoji.js
+++ src/types/custom-emoji.js
@@ -8,11 +8,13 @@ class CustomEmojiArgumentType extends ArgumentType {
 	}
 
 	validate(value, msg) {
-		const matches = value.match(/^(?:<a?:([a-zA-Z0-9_]+):)?([0-9]+)>?$/);
-		if(matches && msg.client.emojis.cache.has(matches[2])) return true;
-		if(!msg.guild) return false;
-		const search = value.toLowerCase();
-		let emojis = msg.guild.emojis.cache.filter(nameFilterInexact(search));
+		const matches = value.match(/^(?:(?:<a?:([^\s]+):([0-9]+)>)|(?::([^\s]+):)|([0-9]+))$/);
+		if(matches) {
+			if(msg.client.emojis.cache.has(matches[2])) return true;
+			if(msg.client.emojis.cache.has(matches[4])) return true;
+		}
+		const search = (matches[1] || matches[3] || value).toLowerCase();
+		let emojis = msg.client.emojis.cache.filter(nameFilterInexact(search));
 		if(!emojis.size) return false;
 		if(emojis.size === 1) return true;
 		const exactEmojis = emojis.filter(nameFilterExact(search));
@@ -24,10 +26,13 @@ class CustomEmojiArgumentType extends ArgumentType {
 	}
 
 	parse(value, msg) {
-		const matches = value.match(/^(?:<a?:([a-zA-Z0-9_]+):)?([0-9]+)>?$/);
-		if(matches) return msg.client.emojis.cache.get(matches[2]) || null;
-		const search = value.toLowerCase();
-		const emojis = msg.guild.emojis.cache.filter(nameFilterInexact(search));
+		const matches = value.match(/^(?:(?:<a?:([^\s]+):([0-9]+)>)|(?::([^\s]+):)|([0-9]+))$/);
+		if(matches) {
+			if(msg.client.emojis.cache.has(matches[2])) return msg.client.emojis.cache.get(matches[2]) || null;
+			if(msg.client.emojis.cache.has(matches[4])) return msg.client.emojis.cache.get(matches[4]) || null;
+		}
+		const search = (matches[1] || matches[3] || value).toLowerCase();
+		const emojis = msg.client.emojis.cache.filter(nameFilterInexact(search));
 		if(!emojis.size) return null;
 		if(emojis.size === 1) return emojis.first();
 		const exactEmojis = emojis.filter(nameFilterExact(search));
