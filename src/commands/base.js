f1ff9f5c937efd1b2ee0ef25c7b11f60d62a1e19
Dragon Fire <danielbodendahl@gmail.com>
2021-02-07 11:29:34 -0500

    Replace `` with just `

--- src/commands/base.js
+++ src/commands/base.js
@@ -480,17 +480,17 @@ class Command {
 	 */
 	static usage(command, prefix = null, user = null) {
 		const nbcmd = command.replace(/ /g, '\xa0');
-		if(!prefix && !user) return `\`\`${nbcmd}\`\``;
+		if(!prefix && !user) return `\`${nbcmd}\``;
 
 		let prefixPart;
 		if(prefix) {
 			if(prefix.length > 1 && !prefix.endsWith(' ')) prefix += ' ';
 			prefix = prefix.replace(/ /g, '\xa0');
-			prefixPart = `\`\`${prefix}${nbcmd}\`\``;
+			prefixPart = `\`${prefix}${nbcmd}\``;
 		}
 
 		let mentionPart;
-		if(user) mentionPart = `\`\`@${user.username.replace(/ /g, '\xa0')}#${user.discriminator}\xa0${nbcmd}\`\``;
+		if(user) mentionPart = `\`@${user.username.replace(/ /g, '\xa0')}#${user.discriminator}\xa0${nbcmd}\``;
 
 		return `${prefixPart || ''}${prefix && user ? ' or ' : ''}${mentionPart || ''}`;
 	}
