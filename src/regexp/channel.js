2f7eedbcdd50fc63012335ca8738df997cb8c15c
Maxim Shamber <36640388+nowifi4u@users.noreply.github.com>
2021-04-01 16:40:18 +0300

    Update regex to accomodate canary and prb users

--- src/regexp/channel.js
+++ src/regexp/channel.js
@@ -1,14 +1,14 @@
 exports.RegExp = {
 	ping: () => /<#([0-9]{7,})>/,
-	linkGuild: () => /(?:https?:\/\/)?discord.com\/channels\/([0-9]{7,})\/([0-9]{7,})\/?/,
-	linkDM: () => /(?:https?:\/\/)?discord.com\/channels\/@me\/([0-9]{7,})\/?/,
-	link: () => /(?:https?:\/\/)?discord.com\/channels\/([0-9]{7,}|@me)\/([0-9]{7,})\/?/,
+	linkGuild: () => /(?:https?:\/\/)?(?:ptb\.|canary\.)?discord\.com\/channels\/([0-9]{7,})\/([0-9]{7,})\/?/,
+	linkDM: () => /(?:https?:\/\/)?(?:ptb\.|canary\.)?discord\.com\/channels\/@me\/([0-9]{7,})\/?/,
+	link: () => /(?:https?:\/\/)?(?:ptb\.|canary\.)?discord\.com\/channels\/([0-9]{7,}|@me)\/([0-9]{7,})\/?/,
 	linkChannel: channelid => new RegExp(`(?:https?:\\/\\/)?discord.com\\/channels\\/(${channelid})\\/([0-9]{7,})\\/?`)
 };
 
 exports.RegExpSingle = {
 	ping: () => /^<#([0-9]{7,})>$/,
-	linkGuild: () => /^(?:https?:\/\/)?discord.com\/channels\/([0-9]{7,})\/([0-9]{7,})\/?$/,
-	linkDM: () => /^(?:https?:\/\/)?discord.com\/channels\/@me\/([0-9]{7,})\/?$/,
-	link: () => /^(?:https?:\/\/)?discord.com\/channels\/([0-9]{7,}|@me)\/([0-9]{7,})\/?$/
+	linkGuild: () => /^(?:https?:\/\/)?(?:ptb\.|canary\.)?discord\.com\/channels\/([0-9]{7,})\/([0-9]{7,})\/?$/,
+	linkDM: () => /^(?:https?:\/\/)?(?:ptb\.|canary\.)?discord\.com\/channels\/@me\/([0-9]{7,})\/?$/,
+	link: () => /^(?:https?:\/\/)?(?:ptb\.|canary\.)?discord\.com\/channels\/([0-9]{7,}|@me)\/([0-9]{7,})\/?$/
 };
