4b0804c31b4cc1e081116cce3777a8bc4931805d
rob9315 <dev.robk@gmail.com>
2021-08-10 22:56:30 +0200

    IEize

--- src/util.js
+++ src/util.js
@@ -34,9 +34,6 @@ function paginate(items, page = 1, pageLength = 10) {
 		pageLength
 	};
 }
-// This returns Object.prototype in order to return a valid object
-// without creating a new one each time this is called just to discard it the moment after.
-const isConstructorProxyHandler = { construct() { return Object.prototype; } };
 
 function isConstructor(func, _class) {
 	try {
